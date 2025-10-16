35421517e277fd842d1a5d45453c27b45d0fba43
george <tomiuc117@gmail.com>
2025-10-16 18:35:41 +0300

    react gh pages config

--- react/00-full/example/react-example/src/components/Logos/components/Logo/Logo.tsx
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import type {ReactNode} from "react";
-
-export default function Logo({href, children, className = ""}: { href: string, children: ReactNode; className?: string }) {
-    return <a href={href} target="_blank" className={"logo " + className}>
-        {children}
-    </a>
-}
